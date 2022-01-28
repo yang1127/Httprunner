786a684bcb47cd4b774be40001a46917af80e1e9
yang761127 <1536520643@qq.com>
2022-01-28 14:39:53 +0800

    test

--- run.py
+++ run.py
@@ -14,8 +14,20 @@ if __name__=="__main__":
     # cmd = ''' cat allure-report/data/suites.csv | grep -v passed | grep -v Status  | awk -F ',' '{print $1,$10}' > logs/error.log'''
     # os.system(cmd)
 
-    # 执行case
-    cmd = 'pytest  ./testcases --clean-alluredir  --alluredir=results'
-    os.system(cmd)
-    cmd = 'allure generate results -o ./reports --clean'
-    os.system(cmd)
+    def env_clear():
+        path = os.getcwd()
+        if os.path.exists(path + "/results"):
+            os.system("rm -rf results")
+        if os.path.exists(path + "/reports"):
+            os.system("rm -rf reports")
+
+
+    if __name__ == "__main__":
+        # 清除上次报告
+        env_clear()
+
+        # 执行case
+        cmd = 'pytest  ./testcases --clean-alluredir  --alluredir=results'
+        os.system(cmd)
+        cmd = 'allure generate results -o ./reports --clean'
+        os.system(cmd)
